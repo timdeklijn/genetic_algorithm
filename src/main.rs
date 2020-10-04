f1f1870e328529885e4e87e9653810d269e76843
Tim de Klijn <timdeklijn@gmail.com>
2020-10-04 14:18:21 +0200

    Refactor Algorithms

--- src/main.rs
+++ src/main.rs
@@ -3,149 +3,214 @@ use rand::Rng;
 
 const ANSWER: &str = "To be, or not to be!";
 const MUTATION_RATE: f64 = 0.03;
+const POP_SIZE: usize = 500;
+const GENERATIONS: usize = 1000;
 
-fn pop_printer(population: &Vec<Vec<char>>) {
-    for i in population {
-        let s: String = i.into_iter().collect();
-        if s == ANSWER {
-            println!("-- {}", s)
-        }
+#[derive(Clone, Debug)]
+struct Genes {
+    genes: Vec<char>,
+    size: usize,
+}
+
+impl Genes {
+    fn new(genes: Vec<char>, size: usize) -> Genes {
+        Genes { genes, size }
+    }
+
+    fn create_individual_genome(self) -> DNA {
+        let dna = self
+            .genes
+            .choose_multiple(&mut rand::thread_rng(), self.size)
+            .cloned()
+            .collect();
+        DNA::new(dna)
     }
 }
 
-fn calculate_fitness(population: &Vec<Vec<char>>) -> Vec<f32> {
-    // Claculate fitness
-    let mut fitness = Vec::new();
-    for i in 0..population.len() {
-        // Per DNA sequence check similarity with answer
+#[derive(Clone, Debug)]
+struct DNA {
+    dna: Vec<char>,
+}
+
+impl DNA {
+    fn new(dna: Vec<char>) -> Self {
+        DNA { dna }
+    }
+
+    fn calc_fitness(&self) -> f32 {
         let mut f = 0;
-        for (i, gene) in population[i].clone().into_iter().enumerate() {
+        let d = self.dna.clone();
+        for (i, gene) in d.into_iter().enumerate() {
             if gene == ANSWER.chars().nth(i).unwrap() {
                 f += 1
             }
         }
-        fitness.push(f as f32 / ANSWER.len() as f32);
+        f as f32 / ANSWER.len() as f32
     }
-    fitness
 }
 
-fn new_dna(p1: &Vec<char>, p2: &Vec<char>, genes: &Vec<char>) -> Vec<char> {
-    let mut rng = rand::thread_rng();
-    let mut new_dna = Vec::new();
-    for i in 0..p1.len() {
-        new_dna.push(if rng.gen::<f64>() >= 0.5 {
-            p1[i].clone()
-        } else {
-            p2[i].clone()
-        })
+#[derive(Clone, Debug)]
+struct Individual {
+    dna: DNA,
+    fitness: f32,
+}
+
+impl Individual {
+    fn new(dna: DNA) -> Self {
+        let fitness = dna.calc_fitness();
+        Individual { dna, fitness }
     }
-    let mut mutated_dna = Vec::new();
-    for i in 0..p1.len() {
-        if rng.gen::<f64>() < MUTATION_RATE {
-            mutated_dna.push(genes.choose(&mut rng).unwrap().clone())
-        } else {
-            mutated_dna.push(new_dna[i])
+}
+
+#[derive(Debug, Clone)]
+struct Population {
+    pop: Vec<Individual>,
+    genes: Genes,
+}
+impl Population {
+    fn new(genes: Genes) -> Self {
+        let mut pop = Vec::new();
+        for _ in 0..POP_SIZE {
+            let genes = genes.clone(); // TODO: Ugly, replace!
+            let dna = genes.create_individual_genome();
+            let i = Individual::new(dna);
+            pop.push(i)
         }
+        Population { pop, genes }
     }
-    mutated_dna
-}
 
-fn probability_vector(l: &Vec<f32>) -> Vec<usize> {
-    // Check if l has only zeros
-    let all_zero: f32 = l
-        .iter()
-        .filter(|&x| x.clone() != 0.0)
-        .fold(0.0, |a, b| a + b);
-    // If l has only zeros, make them only 0.1's
-    let l: Vec<f32> = if all_zero == 0.0 {
-        l.clone().iter().map(|_| 0.1).collect()
-    } else {
-        l.clone()
-    };
-
-    // Find smallest value in the list (ignoring the zeros)
-    let min_value = l
-        .iter()
-        .filter(|a| a.fract() != 0.0) // remove zeros
-        .fold(f32::INFINITY, |a, &b| a.min(b)); // find minimum
-    let factor = (1.0 / min_value).ceil();
-
-    // Fill a list with indices, based on their probability
-    let mut p_vec = Vec::new();
-    for i in 0..l.len() {
-        if l[i] > 0.0 {
-            let n = (l[i] * factor).floor() as usize;
-            for _ in 0..n {
-                p_vec.push(i)
+    fn pop_printer(&self) {
+        for individual in self.pop.iter() {
+            let s: String = individual.dna.dna.clone().into_iter().collect();
+            if s == ANSWER {
+                println!("-- {}", s)
             }
         }
     }
-    p_vec
-}
 
-fn choose_parents_index(p_vec: Vec<usize>) -> (usize, usize) {
-    let p1: usize = match p_vec.choose(&mut rand::thread_rng()) {
-        Some(x) => x.clone(),
-        None => panic!(),
-    };
-    if p_vec.len() == 1 {
-        return (p1, p1);
+    fn get_max_fitness(&self) -> f32 {
+        let fitness_vec = self.get_fitness_vector();
+        fitness_vec.iter().fold(-f32::INFINITY, |a, &b| a.max(b))
     }
-    let p2: usize = loop {
-        let pp = match p_vec.choose(&mut rand::thread_rng()) {
+
+    fn get_fitness_vector(&self) -> Vec<f32> {
+        self.pop.iter().map(|x| x.fitness.clone()).collect()
+    }
+
+    fn calc_probability_vector(&self) -> Vec<usize> {
+        // Check if l has only zeros
+        let fitness_vec = self.get_fitness_vector();
+        let all_zero: f32 = fitness_vec
+            .iter()
+            .filter(|&x| x.clone() != 0.0)
+            .fold(0.0, |a, b| a + b);
+        // If l has only zeros, make them only 0.1's
+        let fitness_vec: Vec<f32> = if all_zero == 0.0 {
+            fitness_vec.clone().iter().map(|_| 0.1).collect()
+        } else {
+            fitness_vec.clone()
+        };
+
+        // Find smallest value in the list (ignoring the zeros)
+        let min_value = fitness_vec
+            .iter()
+            .filter(|a| a.fract() != 0.0) // remove zeros
+            .fold(f32::INFINITY, |a, &b| a.min(b)); // find minimum
+        let factor = (1.0 / min_value).ceil();
+
+        // Fill a list with indices, based on their probability
+        let mut p_vec = Vec::new();
+        for i in 0..fitness_vec.len() {
+            if fitness_vec[i] > 0.0 {
+                let n = (fitness_vec[i] * factor).floor() as usize;
+                for _ in 0..n {
+                    p_vec.push(i)
+                }
+            }
+        }
+        p_vec
+    }
+
+    fn choose_parents_index(&self, p_vec: &Vec<usize>) -> (usize, usize) {
+        let p1: usize = match p_vec.choose(&mut rand::thread_rng()) {
             Some(x) => x.clone(),
             None => panic!(),
         };
-        if pp != p1 {
-            break pp;
+        if p_vec.len() == 1 {
+            return (p1, p1);
         }
-    };
-    (p1, p2)
+        let p2: usize = loop {
+            let pp = match p_vec.choose(&mut rand::thread_rng()) {
+                Some(x) => x.clone(),
+                None => panic!(),
+            };
+            if pp != p1 {
+                break pp;
+            }
+        };
+        (p1, p2)
+    }
+
+    fn new_dna(&self, p1: usize, p2: usize) -> Vec<char> {
+        let p1 = self.pop[p1].dna.dna.clone(); //TODO: Ugly
+        let p2 = self.pop[p2].dna.dna.clone();
+
+        let mut rng = rand::thread_rng();
+        let mut new_dna = Vec::new();
+        for i in 0..p1.len() {
+            new_dna.push(if rng.gen::<f64>() >= 0.5 {
+                p1[i].clone()
+            } else {
+                p2[i].clone()
+            })
+        }
+        let mut mutated_dna = Vec::new();
+        for i in 0..p1.len() {
+            if rng.gen::<f64>() < MUTATION_RATE {
+                mutated_dna.push(self.genes.genes.choose(&mut rng).unwrap().clone())
+            } else {
+                mutated_dna.push(new_dna[i])
+            }
+        }
+        mutated_dna
+    }
+
+    fn evolve(&mut self) -> Vec<Individual> {
+        let mut new_pop = Vec::new();
+        let p_vec = self.calc_probability_vector();
+        for _ in 0..POP_SIZE {
+            let (p1, p2) = self.choose_parents_index(&p_vec);
+            let new_dna = self.new_dna(p1, p2);
+            let dna = DNA::new(new_dna);
+            new_pop.push(Individual::new(dna));
+        }
+        new_pop
+    }
+
+    fn run(&mut self) {
+        for g in 0..GENERATIONS {
+            self.pop = self.evolve();
+            let max_fitness = self.get_max_fitness();
+            if g % 100 == 0 || max_fitness == 1.0 {
+                println!("{}: {}", g, max_fitness);
+                self.pop_printer();
+                if max_fitness == 1.0 {
+                    break;
+                }
+            }
+        }
+    }
 }
 
 fn main() {
-    // Answer Length, used often
-    let l = ANSWER.len();
-
     // Initiate possible genes
-    let genes: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.?!'; "
+    let gene_options: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.?!'; "
         .chars()
         .collect();
-
-    let mut population = Vec::new();
-    let pop_size = 100;
-    for _ in 0..pop_size {
-        // Random sequence of length l picked from genes
-        let dna: Vec<char> = genes
-            .choose_multiple(&mut rand::thread_rng(), l)
-            .cloned()
-            .collect();
-        population.push(dna)
-    }
-
-    for generation in 0..1000 {
-        // Claculate fitness
-        let fitness = calculate_fitness(&population);
-
-        // Print - and break when needed
-        let max_fitness = fitness.iter().fold(-f32::INFINITY, |a, &b| a.max(b));
-        if generation % 10 == 0 || max_fitness == 1.0 {
-            println!("generation {}: {}", generation, max_fitness);
-            pop_printer(&population);
-            if max_fitness == 1.0 {
-                break;
-            }
-        }
-
-        // Create new generation
-        let mut new_pop = Vec::new();
-        let f = fitness.clone();
-        for _ in 0..pop_size {
-            let p_vec = probability_vector(&f);
-            let (p1, p2) = choose_parents_index(p_vec);
-            let new_dna = new_dna(&population[p1], &population[p2], &genes);
-            new_pop.push(new_dna);
-        }
-        population = new_pop;
-    }
+    // Create Genes object
+    let genes = Genes::new(gene_options, ANSWER.len());
+    // Create population
+    let mut population = Population::new(genes);
+    // Run the algorithm
+    population.run();
 }
